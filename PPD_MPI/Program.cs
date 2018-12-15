44eeba875960a3fb5cedc188655a95d737dceefc
Madalina Mircea <mirceamariamadalina@yahoo.com>
2018-12-15 19:56:57 +0200

    First try

---
+++ PPD_MPI/Program.cs
@@ -0,0 +1,25 @@
+﻿using MPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPD_MPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(Communicator.world.Rank == 0)
+            {
+                Communicator.world.Send("Hello", 1, 0);
+            }
+            else if(Communicator.world.Rank == 1)
+            {
+                string msg = Communicator.world.Receive<String>(0, 0);
+                Console.WriteLine(msg);
+            }
+        }
+    }
+}
